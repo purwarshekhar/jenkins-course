523464cfa5d5a391d6508fa5e56dddcdfb631c92
purwarshekhar <purwar.shekhar@gmail.com>
2021-05-18 16:22:54 -0400

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('purwarshekhar/jenkins/')
+            repositoryName('purwarshekhar/jenkins')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
